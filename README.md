a0eb02894c26c911f825cfabd5bd07f165d4741b
Dongli99 <dongliliu0@gmail.com>
2024-02-05 10:57:51 -0800

    added readme

---
+++ README.md
@@ -0,0 +1,153 @@
+<h1 align="center">CashCard Spring Boot Practice<h1>
+
+<div align="center">
+  <img src="https://img.shields.io/badge/spring boot-0D1129.svg?style=for-the-badge&logo=springboot&logoColor=%6DB33F" alt="Spring Boot">
+  <img src="https://img.shields.io/badge/junit-BE3939.svg?style=for-the-badge&logo=junit5&logoColor=white" alt="Java">
+</div>
+
+# Overview
+
+The repo is following the course [Building a REST API with Spring Boot](https://spring.academy/courses/building-a-rest-api-with-spring-boot) to get hands-on practice.
+
+# Course Outcomes
+
+- Build a fully functional `REST API` with Spring Boot.
+- Utilize `Spring Security` to authenticate and authorize your application.
+- Implement `test-driven development`.
+- Convert database and Java objects automatically using `Spring Data JDBC`.
+
+# Knowledge Explored
+
+*As of February 5th*
+
+## Concepts
+
+### Main idea
+
+- Context & Dependency Injection
+- Database Access
+- Spring MVC
+
+### IoC (Inversion of Control)
+
+- Inversion of Control Container
+- Providing Dependencies
+- Initializer
+
+### API Contracts & JSON
+
+- API Contracts
+  - Patterns for API Behavior
+  - Consumer & Provider Driven Contracts
+- JSON
+  - JavaScript Object Notation
+
+### Testing
+
+- Red-Green-Refactor
+
+### REST in Spring Boot
+
+- Spring Beans
+- Spring Annotations
+- Spring Web Controllers
+- Implementing GET
+- Implementing POST
+  - Idempotence and HTTP
+  - Request and Response
+
+### Repositories & Spring Data
+
+- Controller-Repository Architecture
+- H2 In-Memory Database
+- Spring Data’s CrudRepository
+
+### Security
+
+- Authentication
+- Spring Security
+  - Filter Chain
+- Authorization
+  - Role-Based Access Control (RBAC)
+- Same Origin Policy
+- Cross-Origin Resource Sharing
+- Common Web Exploits
+  - CSRF & XSS
+
+## Code Analysis
+
+### Components
+
+#### APP
+
+- `SpringApplication.run(CashCardApplication.class, args);`
+
+#### CRUD
+
+- CrudRepository
+- ResponseEntity
+- TestRestTemplate
+- HttpStatus
+
+#### DATA
+
+- Pageable
+- Page<CashCard>
+- PageRequest
+- Sort
+
+#### TEST
+
+- assertThat
+
+#### UTIL
+
+- URI and UriComponentsBuilder
+- Optional
+- DocumentContext (JsonPath)
+- JacksonTester
+
+### Annotations
+
+#### Class Level
+
+- @SpringBootApplication
+- @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+- @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+- @JsonTest
+- @RestController
+- @RequestMapping("/cashcards")
+
+#### Class Variable
+
+- @Autowired
+
+#### Class Method
+
+- @BeforeEach
+- @Test
+- @GetMapping("/{requestedId}")
+- @PostMapping
+- @DirtiesContext
+
+#### Parameter Level
+
+- @PathVariable
+- @RequestBody
+- @Id
+
+## File Analysis
+
+### build.gradle
+
+- Dependencies for Spring Boot and H2 Database
+- Test configurations for more verbose output
+- Resource directories for main and test
+
+### schema.sql
+
+- SQL script defining a table `cash_card`
+
+### data.sql
+
+- SQL script inserting data into `cash_card` table
--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -2,9 +2,7 @@ package example.cashcard;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
-
 import net.minidev.json.JSONArray;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,14 +10,12 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 import java.net.URI;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.annotation.DirtiesContext.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-// @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class CashCardApplicationTests {
 	@Autowired
 	TestRestTemplate restTemplate;
@@ -27,11 +23,12 @@ class CashCardApplicationTests {
 	@Test
 	void shouldReturnACashCardWhenDataIsSaved() {
 		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
-
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
 		DocumentContext documentContext = JsonPath.parse(response.getBody());
 		Number id = documentContext.read("$.id");
 		assertThat(id).isEqualTo(99);
+
 		Double amount = documentContext.read("$.amount");
 		assertThat(amount).isEqualTo(123.45);
 	}
@@ -39,6 +36,7 @@ class CashCardApplicationTests {
 	@Test
 	void shouldNotReturnACashCardWithAnUnknownId() {
 		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 		assertThat(response.getBody()).isBlank();
 	}
@@ -46,7 +44,7 @@ class CashCardApplicationTests {
 	@Test
 	@DirtiesContext
 	void shouldCreateANewCashCard() {
-		CashCard newCashCard = new CashCard(null, 250.00);
+		CashCard newCashCard = new CashCard(null, 250.00, "sarah1");
 		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
 		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
@@ -57,6 +55,7 @@ class CashCardApplicationTests {
 		DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
 		Number id = documentContext.read("$.id");
 		Double amount = documentContext.read("$.amount");
+
 		assertThat(id).isNotNull();
 		assertThat(amount).isEqualTo(250.00);
 	}
@@ -74,21 +73,30 @@ class CashCardApplicationTests {
 		assertThat(ids).containsExactlyInAnyOrder(99, 100, 101);
 
 		JSONArray amounts = documentContext.read("$..amount");
-		assertThat(amounts).containsExactlyInAnyOrder(123.45, 1.0, 150.00);
+		assertThat(amounts).containsExactlyInAnyOrder(123.45, 1.00, 150.00);
 	}
 
 	@Test
 	void shouldReturnAPageOfCashCards() {
-		ResponseEntity<String> response = restTemplate.getForEntity(
-				"/cashcards?page=0&size=1&sort=amount,desc",
-				String.class);
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards?page=0&size=1", String.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
 		DocumentContext documentContext = JsonPath.parse(response.getBody());
 		JSONArray page = documentContext.read("$[*]");
 		assertThat(page.size()).isEqualTo(1);
+	}
 
-		double amount = documentContext.read("[0].amount");
+	@Test
+	void shouldReturnASortedPageOfCashCards() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards?page=0&size=1&sort=amount,desc",
+				String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		JSONArray read = documentContext.read("$[*]");
+		assertThat(read.size()).isEqualTo(1);
+
+		double amount = documentContext.read("$[0].amount");
 		assertThat(amount).isEqualTo(150.00);
 	}
 
